83c331230b4aebece4d320aa0c1933a5a3d18625
Ivan Shity <Ivan.Shity@softengi.com>
2018-05-11 12:27:31 +0300

    add security layer

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // Intercom agent
 
 //var bodyParser = require('body-parser')
-var http = require('http')
+var http = require('http');
 var exp = require('express');
-var mqtt = require('mqtt')
-var request = require('request')
+var mqtt = require('mqtt');
+var request = require('request');
 
 var app = exp();
 //app.use(bodyParser.urlencoded({ extended: true}));
@@ -13,7 +13,14 @@ var options = {
 	host:"localhost",
 	port:1883
 }
-
+var url_options = {
+        'url': 'http://192.168.0.105/api/camera/snapshot?width=640&height=480&source=internal',
+        'auth': {
+            'user': 'admin',
+            'pass': 'admin',
+            'sendImmediately': false
+        }
+    };
 
 // Starting web server for listen POST request
 var server = app.listen(8080, () => {
@@ -28,6 +35,7 @@ var client = mqtt.connect(options)
 client.on("connect", () => {
 	console.log("Succefully connected to mqtt bridge")
 })
+
 // Main function to handle GET request and call to client
 app.get('/*', (req, res) => {
         const body = req.params['0']
@@ -35,21 +43,34 @@ app.get('/*', (req, res) => {
         console.log(body)
         res.send("OK")
         try {
-        	agent.publish("bridge/intercom_call/value", body)
+        	client.publish("bridge/intercom_call/value", body)
         }
         catch (err) {
         	console.log(err)
         }
 })
 
-// Send https request to switch door
-
+// Recieve message from client and send https request in order to switch door
+// TODO add certificate security and login/pass autorisation
 client.subscribe('bridge/intercom_answer/command/set-value');
 client.on("message", (topic, payload) => {
         const obj = JSON.parse(payload)
         console.log("Get message via wss: ", obj)
+        // add condition
         request.get("http://192.168.0.105/api/switch/ctrl?switch=1&action=on",(err, res, body) => {
                 console.log('statusCode:', res && res.statusCode)
                 })
-        client.end()
+})
+
+// Snapshot from camera and sent to client as a picture
+
+client.subscribe('bridge/intercom_snapshot/command/set-value');
+client.on("message", (topic, payload) => {
+        const obj = JSON.parse(payload)
+        request.get(url_options, (err, res, body) => {
+                console.log('statusCode:', res && res.statusCode)
+                const buf = Buffer.from(body, "base64")
+            //    console.log(buf)
+                client.publish("bridge/intercom_snapshot/value", body)
+                })      
 })
